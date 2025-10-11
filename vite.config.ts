21c80c83b64dcc89ad4cae061cd4f35916b6e44c
abhi9vaidya <abhinavvaidya2005@gmail.com>
2025-10-11 15:37:51 +0530

    Initial commit: project files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-// removed lovable-tagger import
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
